c00425f4490635b69b87753bfdaa26c570e0a4ec
Ardi Sasongko <ardisasongko002@gmail.com>
2024-05-20 20:33:00 +0700

    db migrations

---
+++ db/migrations/20240520132310_minimarket_table.up.sql
@@ -0,0 +1,20 @@
+CREATE TABLE users (
+    user_id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    role VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE products (
+    product_id SERIAL PRIMARY KEY,
+    seller_id_fk INTEGER NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    quantity INTEGER NOT NULL,
+    price INTEGER NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT seller_fk FOREIGN KEY (seller_id_fk) REFERENCES users(user_id)
+);
